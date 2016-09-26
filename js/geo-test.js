62a948178ec9ca30ae0c7ad45788fe5363649f0d
Brendan Cecere <brendancecere@gmail.com>
2016-09-26 11:27:33 -0400

    first commit

---
+++ js/geo-test.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Brendan on 9/26/2016.
+ */
+var x = document.getElementById("demo");
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude;
+}
