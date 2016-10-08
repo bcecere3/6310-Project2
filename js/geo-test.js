99ebbcde6ab5b752cd156b821c8c190c446ebb73
Brendan Cecere <brendancecere@gmail.com>
2016-10-08 19:14:31 -0400

    switched over to template to fix header

---
+++ js/geo-test.js
@@ -0,0 +1,39 @@
+/**
+ * Created by Brendan on 9/26/2016.
+ */
+var x = document.getElementById("demo");
+var coord = {};
+var piedmont = {"SW": [33.781807, -84.378261], "NW": [33.787427, -84.378041], "NE": [33.786195, -84.370759], "SE":[33.781949, -84.368649]};
+$(document).ready(function () {
+    $(".button-collapse").sideNav();
+});
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+function showPosition(position) {
+    // console.log(piedmont);
+    coord.latitude = position.coords.latitude;
+    coord.longitude = position.coords.longitude;
+    checkPiedmont(coord);
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude;
+}
+function checkPiedmont(c){
+    var lat = c.latitude;
+    var long = c.longitude;
+    if(long >= piedmont.SW[1] && long <= piedmont.SE[1]){
+        if(lat >= piedmont.SW[0] && lat <= piedmont.NW[0]){
+            console.log("you are in piedmont park");
+        }
+        else{
+            console.log("wrong long")
+        }
+    }
+    else{
+        console.log("wrong lat");
+    }
+}
